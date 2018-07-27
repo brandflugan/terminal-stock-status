ceb667c773ee662a3cbd0e687a314a79e2c587bb
Markus Bjerre (lgv900) <markus.bjerre@teliacompany.com>
2018-07-27 13:22:17 +0200

    Refactor and simplify code

--- index.js
+++ index.js
@@ -1,53 +1,54 @@
-let request = require('request');
+let rp = require('request-promise-native');
+require('clear')();
 
-const stocks = [
-    'KAN.ST',
-    'TOBII.ST',
-    'STAR-B.ST',
-    'ACTI.ST',
-    'BIOA-B.ST',
-    'BINV.ST'
-];
+const stocksObj = {
+    Kancera: 'KAN.ST',
+    Tobii: 'TOBII.ST',
+    Starbreeze: 'STAR-B.ST',
+    ActiveBiotech: 'ACTI.ST',
+    BioArctic: 'BIOA-B.ST',
+    Oasmia: 'OASM.ST',
+    Fingerprint: 'FING-B.ST'
+};
 
-stocks.forEach(function(stockName) {
-    calculateStockPrice(stockName);
-}, this);
-
-function calculateStockPrice(stockName) {
-    return getPrice(stockName, function(err, prices) {
-        console.log(calculatePrice(stockName, prices));
-    });
+for (const [key, value] of Object.entries(stocksObj)) {
+    calculateStockPrice(key, value);
 }
 
-function calculatePrice (stockName, prices) {
+function calculateStockPrice(company, stockName) {
+    getPrice(stockName)
+        .then(prices => {
+           console.log(printPrice(company, stockName, prices));
+        })
+        .catch(error => {
+           console.log(error);
+        });
+}
+
+function printPrice (company, stockName, prices) {
     let colorCode = '\x1b[0m';
     if (prices.currentPrice >= prices.oldPrice) {
         colorCode = '\x1b[32m';
     } else {
         colorCode = '\x1b[31m';
     }
-    return `\x1b[35m${stockName}: \x1b[36m${prices.currentPrice} ${colorCode}(${calculatePercentage(prices.oldPrice, prices.currentPrice)}%)\x1b[0m`;
+    return `\x1b[37m${company} (${stockName}): \x1b[36m${prices.currentPrice} ${colorCode}(${calculatePercentage(prices.oldPrice, prices.currentPrice)}%)\x1b[0m`;
 }
 
 function calculatePercentage(oldPrice, newPrice) {
     return (((newPrice - oldPrice) / oldPrice) * 100).toFixed(2);
 }
 
-
-function getPrice (stockName, callback) {
-
-    request("https://finance.yahoo.com/quote/" + stockName + "/", function(err, res, body){
-
-		if (err) {
-			callback(err);
-		}
-
-        let prices = {
-            currentPrice: body.split("currentPrice")[1].split("fmt\":\"")[1].split("\"")[0],
-            oldPrice: body.split("previousClose")[1].split("fmt\":\"")[1].split("\"")[0]
-        };
-
-		callback(null, prices);
-
-	});
+function getPrice (stockName) {
+    return rp("https://finance.yahoo.com/quote/" + stockName + "/")
+        .then((body) => {
+            return {
+                currentPrice: body.split("currentPrice")[1].split("fmt\":\"")[1].split("\"")[0],
+                oldPrice: body.split("previousClose")[1].split("fmt\":\"")[1].split("\"")[0]
+            };
+        })
+        .catch((error) => {
+            return error;
+        });
 }
+
